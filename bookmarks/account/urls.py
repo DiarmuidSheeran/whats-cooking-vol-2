5629e0accc1617b37e01fc32b09c169a1c995ce9
Diarmuid Sheeran <dssheeran@outlook.com>
2024-05-01 19:30:23 +0100

    Create dashboard template and add login redierct to settings.py

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # previous login view
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard')
 ]
     
