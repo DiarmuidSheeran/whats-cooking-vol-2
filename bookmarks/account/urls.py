17205e9a4b3b96380203630fa10389049a542526
Diarmuid Sheeran <dssheeran@outlook.com>
2024-05-11 23:37:03 +0100

    Change longin page styles

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,6 +8,14 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+
+    # Change Password Views
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',     
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
 ]
     
